e5bb363417e6d07e4e54298a9ab3d3d48acac33e
Kyrell <kleanosiauw@student.ciputra.ac.id>
2025-10-28 10:29:28 +0700

    style: add spin to Loader component in LoadingPage

--- app/(main)/loading.tsx
+++ app/(main)/loading.tsx
@@ -3,7 +3,7 @@ import { Loader } from "lucide-react";
 export default function LoadingPage() {
   return (
     <div className="w-full h-full grid place-items-center">
-      <Loader className="size-6" />
+      <Loader className="size-6 animate-spin" />
       <br />
       Loading...
     </div>
